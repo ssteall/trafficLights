08d1c46c576d9d57fe8994f5263912eddd0afa15
Dmiroshnikov <ssteamm90@gmail.com>
2022-03-14 02:15:22 +0300

    Init project

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var PressStartButton: UIButton!
+    @IBOutlet var RedLight: UIView!
+    @IBOutlet var YellowLight: UIView!
+    @IBOutlet var GreenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        RedLight.layer.cornerRadius = RedLight.layer.bounds.width / 2
+        YellowLight.layer.cornerRadius = 70
+        GreenLight.layer.cornerRadius = 70
+        
+        RedLight.alpha = 0.3
+        GreenLight.alpha = 0.3
+        YellowLight.alpha = 0.3
     }
 
-
+    @IBAction func PressButton(_ sender: Any) {
+        PressStartButton.setTitle("Next", for: .normal )
+        RedLight.alpha = 1
+        GreenLight.alpha = 1
+        YellowLight.alpha = 1
+    }
+    
 }
 
